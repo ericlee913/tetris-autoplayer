68304297c1c93029fb1149ac8abb6ab2ca9db1a2
ericlee913 <zcabeeg@ucl.ac.uk>
2023-11-17 16:34:44 +0000

    changed algorithm

--- player.py
+++ player.py
@@ -8,46 +8,10 @@ class Player:
         raise NotImplementedError
 
 
-class RandomPlayer(Player):
-    def __init__(self, seed=None):
-        self.random = Random(seed)
-
-    def print_board(self, board):
-        print("--------")
-        for y in range(24):
-            s = ""
-            for x in range(10):
-                if (x,y) in board.cells:
-                    s += "#"
-                else:
-                    s += "."
-            print(s, y)
-                
-
-            
-
-    def choose_action(self, board):
-        self.print_board(board)
-        time.sleep(0.5)
-        if self.random.random() > 0.97:
-            # 3% chance we'll discard or drop a bomb
-            return self.random.choice([
-                Action.Discard,
-                Action.Bomb,
-            ])
-        else:
-            # 97% chance we'll make a normal move
-            return self.random.choice([
-                Direction.Left,
-                Direction.Right,
-                Direction.Down,
-                Rotation.Anticlockwise,
-                Rotation.Clockwise,
-            ])
-
 class JunwoosPlayer(Player):
     def __init__(self, seed=None):
-        self.random = Random(seed)
+        pass
+
 
     def print_board(self, board):
         print("--------")
@@ -59,27 +23,56 @@ class JunwoosPlayer(Player):
                 else:
                     s += "."
             print(s, y)
-                
+
+    
+    def get_heights(self, board):
+        heights = [0] * board.width
+        for x, y in board.cells:
+            heights[x] = max(heights[x], board.height - y)
+        return heights
+
+
+    def score(self, board):
+        heights = self.get_heights(board)
+        return -max(heights)
+
+    
+    def move_to_target(self, board, target_x, target_r):
+        moves = []
+        has_landed = False
+        while not has_landed and target_r != 0:
+            if target_r == 3:
+                moves.append(Rotation.Anticlockwise)
+                has_landed = board.rotate(Rotation.Anticlockwise)
+                break
+            moves.append(Rotation.Clockwise)
+            has_landed = board.rotate(Rotation.Clockwise)
+            target_r -= 1
+        
+        while not has_landed and target_x > board.falling.left:
+            moves.append(Direction.Right)
+            has_landed = board.move(Direction.Right)
+        while not has_landed and target_x < board.falling.left:
+            moves.append(Direction.Left)
+            has_landed = board.move(Direction.Left)
+        
+        if not has_landed:
+            moves.append(Direction.Drop)
+            board.move(Direction.Drop)
+        
+        return moves
+    
 
     def choose_action(self, board):
-        self.print_board(board)
-        time.sleep(0.5)
-        if self.random.random() > 0.90:
-            # 3% chance we'll discard or drop a bomb
-            return self.random.choice([
-                Action.Discard,
-                Action.Bomb,
-            ])
-        else:
-            # 97% chance we'll make a normal move
-            return self.random.choice([
-                Direction.Left,
-                Direction.Right,
-                Direction.Down,
-                Rotation.Anticlockwise,
-                Rotation.Clockwise,
-            ])
-    
-    
+        possibilities = []
+        for r in range(4):
+            for x in range(board.width - (board.falling.right - board.falling.left)):
+                b = board.clone()
+                moves = self.move_to_target(b, x, r)
+                possibilities.append((self.score(b), moves))
+        _, moves = max(possibilities, key=lambda x: x[0])
+        return moves
+
+        
 
 SelectedPlayer = JunwoosPlayer
