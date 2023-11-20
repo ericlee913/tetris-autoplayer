96491409bbdf050831a718f8ce548fda897a097b
ericlee913 <zcabeeg@ucl.ac.uk>
2023-11-20 04:20:09 +0000

    changed algorithm

--- player.py
+++ player.py
@@ -58,9 +58,9 @@ class JunwoosPlayer(Player):
 
     def score(self, board):
         weight_max_height= 0.8
-        weight_hole_penalty= 13
+        weight_hole_penalty= 11
         weight_num_cleared_lines= 1
-        weight_above_holes = -2
+        weight_above_holes = -2.2
         weight_bumpiness = 1.8
 
         heights = self.get_heights(board)
